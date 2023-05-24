b8fa1392ee774c274305552c80f61c5a5998d508
Lukasz <lukasjaniak44@gmail.com>
2023-05-24 15:07:18 +0200

    nav

--- portfolio/src/res/MainPage.js
+++ portfolio/src/res/MainPage.js
@@ -1,15 +1,15 @@
 import { useState, useEffect, useRef } from 'react';
+import Navbar from './Navbar';
 
 const MainPage = () => {
   const [value, setValue] = useState('ala');
 
   return (
     <>
-      <div className="background-site">
-        <div className="background-center-div">
-          <div>{value}</div>
-          <div>{value}</div>
-        </div>
+      <Navbar />
+      <div className="background-center-div">
+        <div>{value}</div>
+        <div>{value}</div>
       </div>
     </>
   );
---
+++ portfolio/src/res/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar-div">
+        <div>Start</div>
+        <div>Tech</div>
+        <div>Sites</div>
+        <div>Kontakt</div>
+        <div>CV</div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
