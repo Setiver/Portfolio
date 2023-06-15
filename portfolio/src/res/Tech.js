bf8cb4eaa9a80f8c2a1235a03daf5567ff993348
Lukasz <lukasjaniak44@gmail.com>
2023-06-15 17:16:06 +0200

    photoshop flic

--- portfolio/src/res/Tech.js
+++ portfolio/src/res/Tech.js
@@ -1,12 +1,12 @@
 import ILLUSTRATOR from './img/icons/Ai.png';
 import PHOTOSHOP from './img/icons/Ps.png';
 import PREMIERE from './img/icons/Pr.png';
+import OFFICE from './img/icons/office.png';
 import HTML from './img/icons/html.png';
 import CSS from './img/icons/css.png';
 import JS from './img/icons/js.png';
 import REACT from './img/icons/react.png';
 import GIT from './img/icons/git.png';
-import OFFICE from './img/icons/office.png';
 
 import Fade from 'react-reveal/Fade';
 import { useState, useEffect } from 'react';
@@ -43,6 +43,10 @@ const Tech = () => {
             <img src={JS} alt="javascript" ref={ref} className={`javascript  ${visible === true ? '' : 'flick'}`} />
             <img src={HTML} alt="html" ref={ref} className={`html  ${visible === true ? '' : 'flick'}`} />
             <img src={CSS} alt="css" ref={ref} className={`css  ${visible === true ? '' : 'flick'}`} />
+            <img src={REACT} alt="react" ref={ref} className={`react  ${visible === true ? '' : 'flick'}`} />
+            <img src={GIT} alt="git" ref={ref} className={`git  ${visible === true ? '' : 'flick'}`} />
+            <img src={PREMIERE} alt="premiere" ref={ref} className={`premiere second-line-icons  ${visible === true ? '' : 'flick'}`} />
+            <img src={PHOTOSHOP} alt="photoshop" ref={ref} className={`photoshop second-line-icons  ${visible === true ? '' : 'flick'}`} />
           </div>
         </Fade>
       </div>
