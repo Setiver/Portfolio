865e86bf5828c022a08b14f050f6e148aab3f0ac
Lukasz <lukasjaniak44@gmail.com>
2023-06-20 13:05:49 +0200

    text

--- portfolio/src/res/Start.js
+++ portfolio/src/res/Start.js
@@ -9,7 +9,10 @@ const Start = () => {
         <img src={BackNeon} alt="bakcneon" />
       </div>
       <div className="about-me">
-        <p>Hi! My name is Łukasz and i'm aspiring Front-End Developer.</p>
+        <p>
+          Hi! My name is Łukasz and i'm aspiring Front-End Developer. I want to develop and acquire new knowledge in this direction, and this page
+          shows my current skills.
+        </p>
       </div>
     </>
   );
